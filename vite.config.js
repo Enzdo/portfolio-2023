2c94bee59a5855af40099fe2c8b7a85ab8dd1f0d
enzdo <enzodeniau2003@gmail.com>
2023-09-04 12:44:47 +0200

    build

--- vite.config.js
+++ vite.config.js
@@ -15,18 +15,26 @@ export default defineConfig({
 
 
   plugins: [
-    
+    {
+      // Ajoutez une règle de traitement spécifique pour les fichiers .glb
+      name: 'glb-loader',
+      enforce: 'pre', // Exécutez ce plugin avant les autres plugins
+      async load(id) {
+        if (id.endsWith('.glb')) {
+          const content = await Deno.readFile(id); // Remplacez Deno par Node.js si vous utilisez Node.js
+          return `export default new Uint8Array(${JSON.stringify([...content])})`;
+        }
+      },
+    },
   ],
 
  
   build: {
 
     rollupOptions: {
+      cssCodeSplit: true, // Active la génération du fichier CSS
       output: {
-        assetFileNames: 'model.glb',
-        assetFileNames2: "sculpture.glb",
-        entryFileNames: 'style.css',
-        main: 'main.js',
+        assetFileNames: '[name].[ext]',
       },
     },
   },
